fc58f1d7190a2228a5c57aa6fa001d85be9f58c4
Rajesh Kumar <rajesh8507kumar@gmail.com>
2025-01-15 22:02:35 +0530

    First Commit

---
+++ src/test/java/com/carservice/carservicebackendservices/CarServiceBackendServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carservice.carservicebackendservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceBackendServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
